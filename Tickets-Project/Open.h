00248985a9144bac847b3491961a17a7d9fa9a30
EmilH06 <emil.chadzidocev@proton.me>
2026-05-10 01:20:03 +0300

    Correction

--- Tickets-Project/Open.h
+++ Tickets-Project/Open.h
@@ -1,10 +1,14 @@
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
-void validInput(std::fstream& file) {
+inline void validInput(std::fstream& file) {
 	if (!file.is_open()) {
 		throw std::invalid_argument("Incorrect input for a filename!");
     }
+	else {
+		std::cout << "Successfully opened file!";
+	}
 }
 class Open {
 	std::fstream file;
@@ -17,5 +21,19 @@ public:
 		catch (std::invalid_argument& e) {
 			std::cout << e.what() << std::endl;
 		}
+	}
+
+	void replicate() {
+		std::ofstream temp("temp.txt", std::ios::out);
+		if (!temp) {
+			std::cout << "Error";
+		}
+		char buffer[256];
+		while (file.getline(buffer, 256)) {
+			temp << buffer;
+		}
+	}
+	~Open() {
+		if (file.is_open()) { file.close(); }
 	}
 };
--- Tickets-Project/main.cpp
+++ Tickets-Project/main.cpp
@@ -1,14 +1,25 @@
+#include "Open.h"
 #include <iostream>
 #include <vector>
 #include <cstring>
 int main() {
 	std::string command;
 	std::string filename;
-	std::string copy;
-	bool access = false;
+	Open* file = nullptr;
 	while (std::cout << "> " && std::cin >> command) {
 		if (command == "open") {
-			std::cin >> filename;
+			if (file != nullptr) {
+				delete[] file;
+			}
+			file = new Open(filename);
+			file->replicate();
+		}
+		else if (command == "exit") {
+			if (file != nullptr) {
+				delete file;
+			}
+			break;
 		}
 	}
+	return 0;
 }
